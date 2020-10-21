1880dd1d4ec9d8f5979948e056957e3f3c655392
samreachyan <samreachyan@gmail.com>
2020-10-22 01:40:05 +0700

    Bai4

---
+++ Thuc Hanh KTLT/Tuan1/Bai4.c
@@ -0,0 +1,26 @@
+/* 
+    auth: Samreach
+    date: 21-Oct-2020
+*/
+#include <stdio.h>
+int counteven(int *arr, int size) {
+    int count = 0;
+    for (int i=0; i<size; i++) {
+        if ((*(arr+i)%2) == 0) {
+            // printf("%d ", *(arr+i));
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int *arr;
+    int size;
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", arr+i);
+    }
+    printf("\n\nCountEven = %d", counteven(arr, size));    
+    return 0;
+}
