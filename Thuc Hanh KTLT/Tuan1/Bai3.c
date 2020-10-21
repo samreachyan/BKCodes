9ee340329be325bfeb145e202d6768e1b713bc5e
samreachyan <samreachyan@gmail.com>
2020-10-22 01:10:31 +0700

    Bai3

---
+++ Thuc Hanh KTLT/Tuan1/Bai3.c
@@ -0,0 +1,27 @@
+/* 
+    auth: Samreach
+    date: 21-Oct-2020
+*/
+#include <stdio.h>
+int main()
+{
+    int x, y, z;
+    int *ptr;
+    scanf("%d %d %d", &x, &y, &z);
+    printf("Here are the values of x, y, and z:\n");
+    printf("%d %d %d\n", x, y, z);
+
+    /*****************
+    # YOUR CODE HERE #
+    *****************/
+    ptr = &x;
+    *ptr += 100;
+    ptr = &y;
+    *ptr += 100;
+    ptr = &z;
+    *ptr += 100;
+    
+    printf("Once again, here are the values of x, y, and z:\n");
+    printf("%d %d %d\n", x, y, z);
+    return 0;
+}   
