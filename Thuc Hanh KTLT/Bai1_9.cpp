03f3cdf1e6f8334f9c66abebd3a80860ab07627f
samreachyan <samreachyan@gmail.com>
2020-10-22 00:28:56 +0700

    bai 1.9

---
+++ Thuc Hanh KTLT/Bai1_9.cpp
@@ -0,0 +1,34 @@
+/* 
+    display string number from index with all length
+    auth: Samreach
+    date: 21-Oct-2020
+*/
+
+#include <iostream>
+using namespace std;
+
+void display (int a[], int index, int n) {
+    int length = 1;
+    while (length <= n) {
+        for (int i=index; i<length; i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        length++;
+    }
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=0; i<n; ++i) {
+        cin >> a[i];
+    }
+
+    for (int i=0 ; i<n; ++i) {
+        display(a, i, n);
+    }
+
+}
