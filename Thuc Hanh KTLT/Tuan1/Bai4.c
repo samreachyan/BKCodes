020321686ebf7b6bc83b39666ccab4d29375d10e
samreachyan <samreachyan@gmail.com>
2020-10-22 11:04:20 +0700

    Bai5

--- Thuc Hanh KTLT/Tuan1/Bai4.c
+++ Thuc Hanh KTLT/Tuan1/Bai4.c
@@ -28,6 +28,34 @@ double* maximum(double* a, int size){
     return max;
 }
 
+//Bai 6
+void reversearray(int arr[], int size){
+    int l = 0, r = size - 1, tmp;
+    for (int i=0; i<=(l+r)/2; ++i) {
+        tmp = arr[l+i];
+        arr[l+i] = arr[r-i];
+        arr[r-i] = tmp;
+    }
+    // print
+    printf("\nRevertArray: \n");
+    for (int i=0; i<=r; ++i) {
+        printf("%d ", arr[i]);
+    }
+}
+
+void ptr_reversearray(int *arr, int size){
+    int l = 0, r = size - 1, tmp;
+    for (int i=l; i<=(l+r)/2; ++i) {
+        tmp = *(arr+(l+i));
+        *(arr+l+i) = *(arr+r-i);
+        *(arr+r-i) = tmp;
+    }
+    printf("\nRevertArray pointer: \n");
+    for (int i=0; i<=r; ++i) {
+        printf("%d ", *(arr+i));
+    }
+}
+
 int main() {
     int *arr;
     int size;
@@ -35,6 +63,8 @@ int main() {
     for (int i = 0; i < size; i++) {
         scanf("%d", arr+i);
     }
+    ptr_reversearray(arr, size);
+
     printf("\nMax = %d", maximum(arr, size));
     printf("\nCountEven = %d", counteven(arr, size));    
     return 0;
