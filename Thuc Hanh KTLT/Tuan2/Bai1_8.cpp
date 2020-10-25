4226219bdf6db5251025650712ff2a5e41aa3a33
samreachyan <samreachyan@gmail.com>
2020-10-26 02:15:38 +0700

    Tuan2

---
+++ Thuc Hanh KTLT/Tuan2/Bai1_8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+bool compare (vector<int> a, vector<int> b) {
+    return accumulate(a.begin(), a.end(), 0) > accumulate(b.begin(), a.end(), 0);
+}
+
+int main() {
+    int val1, val2;
+    cin >> val1 >> val2;
+    vector< vector<int> > a = {
+        {1, 3, 7},
+        {2, 3, 4, val1},
+        {9, 8, 15},
+        {10, val2},
+    };
+
+    sort(a.begin(), a.end(), compare);
+
+    for (const auto &v : a) {
+        for (int it : v) {
+            cout << it << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
