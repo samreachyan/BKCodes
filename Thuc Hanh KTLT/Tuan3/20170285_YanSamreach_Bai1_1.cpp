25420828945f0a023d8dab6c336e8b98f91de7d4
samreachyan <samreachyan@gmail.com>
2020-11-04 02:59:25 +0700

    Tuan3

---
+++ Thuc Hanh KTLT/Tuan3/20170285_YanSamreach_Bai1_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int lucas(int n) {
+    if (n == 0) return 2;
+    if (n == 1) return 1;
+    return lucas(n-1) + lucas(n-2);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << lucas(n) << endl;
+    return 0;
+}
